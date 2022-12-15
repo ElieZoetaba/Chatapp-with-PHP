d78b8338d1022379c6f48310eeb53ab58c0dcd7d
[ElieZoetaba] <[zoetabaelie@gmail.com]>
2022-12-15 13:16:25 +0000

    user and login
    Signed-off-by: [ElieZoetaba] <[zoetabaelie@gmail.com]>

---
+++ php/login.php
@@ -0,0 +1,36 @@
+<?php 
+  session_start();
+  if(isset($_SESSION['unique_id'])){
+    header("location: users.php");
+  }
+?>
+
+<?php include_once "header.php"; ?>
+<body>
+  <div class="wrapper">
+    <section class="form login">
+      <header>Chat App</header>
+      <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+        <div class="error-text"></div>
+        <div class="field input">
+          <label>Adresse e-mail</label>
+          <input type="text" name="email" placeholder="Enter your email" required>
+        </div>
+        <div class="field input">
+          <label>Mots de passe</label>
+          <input type="password" name="password" placeholder="Enter your password" required>
+          <i class="fas fa-eye"></i>
+        </div>
+        <div class="field button">
+          <input type="submit" name="submit" value="Continuer à discuter">
+        </div>
+      </form>
+      <div class="link">Pas encore inscrit? <a href="index.php">S'inscrire maintenant</a></div>
+    </section>
+  </div>
+  
+  <script src="javascript/pass-show-hide.js"></script>
+  <script src="javascript/login.js"></script>
+
+</body>
+</html>
